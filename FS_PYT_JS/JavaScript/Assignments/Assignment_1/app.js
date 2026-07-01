f0fe2de24dc2cf29ec199bc012c87356120ab833
Abdul Hadi <itxhaadi0320@gmail.com>
2026-07-02 00:57:45 +0500

    Added: Variables for Calculator

--- FS_PYT_JS/JavaScript/Assignments/Assignment_1/app.js
+++ FS_PYT_JS/JavaScript/Assignments/Assignment_1/app.js
@@ -8,3 +8,7 @@ console.log(`You can perform following :
     %(modulus)`)
 
 
+// Please enter the desired numbers(any two) and the operator in the following 3 lines to perform your calculation
+let num1 = 2;
+let num2 = 5;
+let operator = "*";
