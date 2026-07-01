011c074067a94293c6c8097ab3b4fc7232221807
Abdul Hadi <itxhaadi0320@gmail.com>
2026-07-02 00:56:45 +0500

    Added: Welcome Statement

---
+++ FS_PYT_JS/JavaScript/Assignments/Assignment_1/app.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Calculator !!!!")
+console.log(`You can perform following : 
+    +(addition), 
+    -(subtraction), 
+    *(multiplication), 
+    /(division), 
+    **(power), 
+    %(modulus)`)
+
+
