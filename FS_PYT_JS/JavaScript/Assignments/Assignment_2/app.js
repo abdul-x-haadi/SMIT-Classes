d25b9ac56c905f6ef3e1935595dc66fc1955af47
Abdul Hadi <itxhaadi0320@gmail.com>
2026-07-02 01:04:13 +0500

    Added: Table 1-5

--- FS_PYT_JS/JavaScript/Assignments/Assignment_2/app.js
+++ FS_PYT_JS/JavaScript/Assignments/Assignment_2/app.js
@@ -3,3 +3,33 @@ let table2 = 2;
 let table3 = 3;
 let table4 = 4;
 let table5 = 5;
+
+console.log("Printing table of 1...")
+for(let i = 1; i<=10; i++){
+    let result = (i*table1)
+    console.log(i, ' x ', table1, ' = ', result);
+}
+
+console.log("Printing table of 2...")
+for(let i = 1; i<=10; i++){
+    let result = (i*table2)
+    console.log(i, ' x ', table2, ' = ', result);
+}
+
+console.log("Printing table of 3...")
+for(let i = 1; i<=10; i++){
+    let result = (i*table3)
+    console.log(i, ' x ', table3, ' = ', result);
+}
+
+console.log("Printing table of 4...")
+for(let i = 1; i<=10; i++){
+    let result = (i*table4)
+    console.log(i, ' x ', table4, ' = ', result);
+}
+
+console.log("Printing table of 5...")
+for(let i = 1; i<=10; i++){
+    let result = (i*table5)
+    console.log(i, ' x ', table5, ' = ', result);
+}   
