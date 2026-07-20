f86001c2e649eda941e68d57c220d5fd9ca71f8d
Abdul Hadi <itxhaadi0320@gmail.com>
2026-07-20 20:15:32 +0500

    Completed : Task 1

---
+++ FS_PYT_JS/JavaScript/Assignments/Assignment_4/app.js
@@ -0,0 +1,17 @@
+// Task 1 – Collect User Information
+// Use *6 prompt()* dialogs to collect user information. For example:
+// * Full Name
+// * Age
+// * City
+// * Profession (Student/Employee)
+// * Email
+// * Phone Number
+
+let fullName = prompt("Enter your full name : ");
+let age = Number(prompt("Enter you age :"));
+let city = prompt("Enter your age : ");
+let profession = prompt("Enter your profession (student/employee) :");
+let email = prompt("Enter your email :");
+let phoneNumber = prompt("Enter your phone number :");
+
+// -----------------------------------------------------------------
