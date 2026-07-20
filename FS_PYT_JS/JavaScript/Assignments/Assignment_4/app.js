6aea4ca347aba9d6585ea3024f5c9ca06cdfcef1
Abdul Hadi <itxhaadi0320@gmail.com>
2026-07-20 20:56:33 +0500

    Completed : Task 2

--- FS_PYT_JS/JavaScript/Assignments/Assignment_4/app.js
+++ FS_PYT_JS/JavaScript/Assignments/Assignment_4/app.js
@@ -9,9 +9,30 @@
 
 let fullName = prompt("Enter your full name : ");
 let age = Number(prompt("Enter you age :"));
-let city = prompt("Enter your age : ");
+let city = prompt("Enter your city : ");
 let profession = prompt("Enter your profession (student/employee) :");
 let email = prompt("Enter your email :");
 let phoneNumber = prompt("Enter your phone number :");
 
 // -----------------------------------------------------------------
+
+// ### Task 2 – Store Data
+// * Create an *object* using the data collected from the prompts.
+// * Store that object inside an *array of objects*.
+// * Save the array in *localStorage* using JSON.stringify().
+
+let personss = JSON.parse(localStorage.getItem("users")) || [];
+let person = {
+    Person_Full_Name :  fullName,
+    Person_Age : age,
+    Person_City : city,
+    Person_Profession : profession,
+    Person_Email : email,
+    Person_Phone_number : phoneNumber
+};
+personss.push(person);
+localStorage.setItem("users", JSON.stringify(personss));
+console.log(personss);
+
+// -----------------------------------------------------------------
+
